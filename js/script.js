c651f5f832e1db2fd94c7f7bb2f3a4e1c02c65ef
JFelixZuniga <johnfzuniga@gmail.com>
2021-04-14 20:52:55 -0400

    About section ok

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function bgAnimationeffect() {
+  const rows = 7;
+  const cols = 10;
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      const div = document.createElement("div");
+      div.className = `col-${j + 1}`;
+      document.querySelector(".bg-animation-effect").appendChild(div);
+    }
+  }
+}
+bgAnimationeffect();
