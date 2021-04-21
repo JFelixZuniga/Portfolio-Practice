ca69dbaac9553cebbaf7d920ad6205e8ef2beea1
JFelixZuniga <johnfzuniga@gmail.com>
2021-04-20 22:40:59 -0400

    Terminando sección de portafolio, y trabajando con JS

--- js/script.js
+++ js/script.js
@@ -11,3 +11,69 @@ function bgAnimationeffect() {
   }
 }
 bgAnimationeffect();
+
+function toggleBodyScrolling() {
+  document.body.classList.toggle("hide-scrolling");
+}
+
+/* Filtro de los items del portafolio */
+const filterBtnsContainer = document.querySelector(".portfolio-filter");
+let portfolioItems;
+filterBtnsContainer.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("portfolio-filter-btn") &&
+    !e.target.classList.contains("active")
+  ) {
+    filterBtnsContainer.querySelector(".active").classList.remove("active");
+    e.target.classList.add("active");
+    toggleBodyScrolling();
+    document.querySelector(".filter-status").classList.add("active");
+    document.querySelector(
+      ".filter-status p"
+    ).innerHTML = `Filtering <span>${e.target.innerHTML}</span> works`;
+    setTimeout(() => {
+      filterItems(e.target);
+    }, 400);
+    setTimeout(() => {
+      document.querySelector(".filter-status").classList.remove("active");
+      toggleBodyScrolling();
+    }, 800);
+  }
+});
+
+function filterItems(filterBtn) {
+  const selectedCategory = filterBtn.getAttribute("data-filter");
+  document.querySelectorAll(".portfolio-item").forEach((item) => {
+    const category = item.getAttribute("data-category").split(",");
+    if (
+      category.indexOf(selectedCategory) !== -1 ||
+      selectedCategory === "all"
+    ) {
+      item.classList.add("show");
+    } else {
+      item.classList.remove("show");
+    }
+  });
+  portfolioItems = document.querySelectorAll(".portfolio-item.show");
+}
+
+filterItems(document.querySelector(".portfolio-filter-btn.active"));
+
+/* Popup detalles */
+
+let portfolioItemIndex;
+document.addEventListener("click", (e) => {
+  if (e.target.closest(".portfolio-item")) {
+    const currentItem = e.target.closest(".portfolio-item");
+    portfolioItemIndex = Array.from(portfolioItems).indexOf(currentItem);
+    togglePopup();
+    console.log(portfolioItemIndex);
+  }
+});
+
+function togglePopup() {
+  document.querySelector(".portfolio-popup").classList.toggle("open")
+  toggleBodyScrolling();
+}
+
+
