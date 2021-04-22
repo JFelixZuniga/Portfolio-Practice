d7d138441e7ce502783fe308e9e4a1ca69ba17ac
JFelixZuniga <johnfzuniga@gmail.com>
2021-04-21 23:05:47 -0400

    sección Portafolio finalizada

--- js/script.js
+++ js/script.js
@@ -67,13 +67,87 @@ document.addEventListener("click", (e) => {
     const currentItem = e.target.closest(".portfolio-item");
     portfolioItemIndex = Array.from(portfolioItems).indexOf(currentItem);
     togglePopup();
-    console.log(portfolioItemIndex);
+    portfolioItemDetails();
+    updateNextPrevItems();
   }
 });
 
 function togglePopup() {
-  document.querySelector(".portfolio-popup").classList.toggle("open")
+  document.querySelector(".portfolio-popup").classList.toggle("open");
   toggleBodyScrolling();
 }
 
+document.querySelector(".pp-close-btn").addEventListener("click", togglePopup);
 
+function portfolioItemDetails() {
+  document.querySelector(".pp-thumbnail img").src = portfolioItems[
+    portfolioItemIndex
+  ].querySelector("img").src;
+
+  document.querySelector(".pp-header h3").innerHTML = portfolioItems[
+    portfolioItemIndex
+  ].querySelector(".portfolio-item-title").innerHTML;
+
+  document.querySelector(".pp-body").innerHTML = portfolioItems[
+    portfolioItemIndex
+  ].querySelector(".portfolio-item-details").innerHTML;
+
+  document.querySelector(".pp-counter").innerHTML = `${
+    portfolioItemIndex + 1
+  } of ${portfolioItems.length}
+  ( <span title="category"> ${
+    document.querySelector(".portfolio-filter-btn.active").innerHTML
+  }</span>)`;
+}
+
+function updateNextPrevItems() {
+  if (portfolioItemIndex !== 0) {
+    document.querySelector(".pp-footer-left").classList.remove("hidden");
+    document.querySelector(".pp-footer-left h3").innerHTML = portfolioItems[
+      portfolioItemIndex - 1
+    ].querySelector("h3").innerHTML;
+    document.querySelector(".pp-footer-left img").src = portfolioItems[
+      portfolioItemIndex - 1
+    ].querySelector("img").src;
+  } else {
+    document.querySelector(".pp-footer-left").classList.add("hidden");
+  }
+
+  if (portfolioItemIndex !== portfolioItems.length - 1) {
+    document.querySelector(".pp-footer-right").classList.remove("hidden");
+    document.querySelector(".pp-footer-right h3").innerHTML = portfolioItems[
+      portfolioItemIndex + 1
+    ].querySelector("h3").innerHTML;
+    document.querySelector(".pp-footer-right img").src = portfolioItems[
+      portfolioItemIndex + 1
+    ].querySelector("img").src;
+  } else {
+    document.querySelector(".pp-footer-right").classList.add("hidden");
+  }
+}
+
+document.querySelector(".pp-prev-btn").addEventListener("click", () => {
+  changePortfolioItem("prev");
+});
+
+document.querySelector(".pp-next-btn").addEventListener("click", () => {
+  changePortfolioItem("next");
+});
+
+function changePortfolioItem(direction) {
+  if (direction == "prev") {
+    portfolioItemIndex--;
+  } else {
+    portfolioItemIndex++;
+  }
+  document.querySelector(".pp-overlay").classList.add(direction);
+  setTimeout(() => {
+    document.querySelector(".pp-inner").scrollTo(0, 0);
+    portfolioItemDetails();
+    updateNextPrevItems();
+  }, 400);
+
+  setTimeout(() => {
+    document.querySelector(".pp-overlay").classList.remove(direction);
+  }, 800);
+}
